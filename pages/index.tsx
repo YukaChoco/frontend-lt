ee1cdd07037af6253b88a021ff8d2c345d8f7148
YukaChoco <sugar.ore.yk@gmail.com>
2024-06-02 22:14:32 +0900

    背景設定

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>hello world</main>
+      <main>
+        <h1>フロントエンドの楽しさ</h1>
+        
+        <p>
+          学生IT団体watnowにおける、新入生向け技術紹介LT会で用いたスライド用ページです。
+        </p>
+      </main>
     </>
   );
 }
