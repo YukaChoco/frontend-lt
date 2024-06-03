729c8224a6354ae4afe034d5513e8b8c163b0bff
YukaChoco <sugar.ore.yk@gmail.com>
2024-06-04 02:19:31 +0900

    mui Provider設定(DarkMode対応)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ThemeProvider>
   );
 }
