492217b67f2a8072f160fa138b09878fb0838d9e
YukaChoco <sugar.ore.yk@gmail.com>
2024-06-04 12:13:25 +0900

    テストお疲れスライド追加

--- components/slides/index.tsx
+++ components/slides/index.tsx
@@ -1,11 +1,12 @@
 import IntroduceSlide from "./introduceSlide";
 import Slide2 from "./slide2";
+import TestOtsukare from "./testOtsukare";
 import TitleSlide from "./titleSlide";
 
 export const slides = [
   <TitleSlide />,
   <IntroduceSlide />,
-  <Slide2 />,
+  <TestOtsukare />,
   <Slide2 />,
   <Slide2 />,
   <Slide2 />,
---
+++ components/slides/testOtsukare.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+export default function TestOtsukare() {
+  return (
+    <Box>
+      <Typography pt={4} fontSize={36} textAlign={"center"}>
+        中間テスト
+        <br />
+        お疲れ様でした！
+      </Typography>
+    </Box>
+  );
+}
